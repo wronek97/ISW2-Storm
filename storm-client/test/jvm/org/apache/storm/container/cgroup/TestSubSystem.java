f0aab12ed0bd4eb5172024278e119a5ed8da3f34
wronek97 <bianchidavide97@gmail.com>
2023-12-16 14:47:50 +0100

    Trying to update to junit5

--- storm-client/test/jvm/org/apache/storm/container/cgroup/TestSubSystem.java
+++ storm-client/test/jvm/org/apache/storm/container/cgroup/TestSubSystem.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.*;
-
 import java.util.Arrays;
 import java.util.Collection;
 
