25683c16ad57403b1a7b3f5e6871abc00cae40c4
wronek97 <bianchidavide97@gmail.com>
2023-12-16 19:43:14 +0100

    update TestSubSystem.java

--- storm-client/test/jvm/org/apache/storm/container/cgroup/TestSubSystem.java
+++ storm-client/test/jvm/org/apache/storm/container/cgroup/TestSubSystem.java
@@ -11,58 +11,86 @@ import java.util.Collection;
 
 public class TestSubSystem {
 
+    private SubSystemType validType = SubSystemType.cpu;
+    private SubSystemType anotherType = SubSystemType.memory;
+    private int positive = 1301;
+    private int negative = -1997;
+
     @Test
     public void testConstructor() {
-        assertTrue(true);
+        SubSystem sys;
+
+        sys = new SubSystem(validType, positive, positive, true);
+        assertNotNull(sys);
+        sys = new SubSystem(validType, positive, positive, false);
+        assertNotNull(sys);
+        sys = new SubSystem(validType, positive, negative, true);
+        assertNotNull(sys);
+        sys = new SubSystem(validType, positive, negative, false);
+        assertNotNull(sys);
+        sys = new SubSystem(validType, negative, positive, true);
+        assertNotNull(sys);
+        sys = new SubSystem(validType, negative, positive, false);
+        assertNotNull(sys);
+        sys = new SubSystem(validType, negative, negative, true);
+        assertNotNull(sys);
+        sys = new SubSystem(validType, negative, negative, false);
+        assertNotNull(sys);
     }
 
     @Test
-    public void testGetType() {
-        assertTrue(true);
+    public void testSetAndGetType() {
+        SubSystem sys = new SubSystem(validType, positive, positive, true);
+
+        assertEquals(validType, sys.getType());
+        sys.setType(anotherType);
+        assertEquals(anotherType, sys.getType());
     }
 
     @Test
-    public void testSetType() {
-        assertTrue(true);
-    }
+    public void testSetAndGetHierarchyID() {
+        SubSystem sys = new SubSystem(validType, positive, positive, true);
 
-    @Test
-    public void testGetHierarchyID() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void testSetHierarchyID() {
-        assertTrue(true);
+        assertEquals(positive, sys.getHierarchyID());
+        sys.setHierarchyID(negative);
+        assertEquals(negative, sys.getHierarchyID());
     }
 
     @Test
     public void testGetCgroupsNum() {
-        assertTrue(true);
+        SubSystem sys = new SubSystem(validType, positive, positive, true);
+
+        assertEquals(positive, sys.getCgroupsNum());
+        sys.setCgroupsNum(negative);
+        assertEquals(negative, sys.getCgroupsNum());
     }
 
     @Test
-    public void testSetCgroupsNum() {
-        assertTrue(true);
-    }
+    public void testSetAndIsEnable() {
+        SubSystem sys = new SubSystem(validType, positive, positive, true);
 
-    @Test
-    public void testIsEnable() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void testSetEnable() {
-        assertTrue(true);
+        assertEquals(true, sys.isEnable());
+        sys.setEnable(false);
+        assertEquals(false, sys.isEnable());
     }
 
     @Test
     public void testHashCode() {
-        assertTrue(true);
+        SubSystem sys = new SubSystem(validType, positive, positive, true);
+
+        assertEquals(31*(31 + type.hashCode())+sys.getHierarchyID(), sys.hashCode());
+        sys = new SubSystem(null, positive, positive, true);
+        assertEquals(31*31+sys.getHierarchyID(), sys.hashCode());
     }
 
     @Test
     public void testEquals() {
-        assertTrue(true);
+        SubSystem sys = new SubSystem(validType, positive, positive, true);
+
+        assertFalse(sys.equals(Integer.valueOf(positive)));
+        assertFalse(sys.equals(null));
+        assertFalse(sys.equals(new SubSystem(anotherType, positive, positive, true)));
+        assertFalse(sys.equals(new SubSystem(validType, negative, positive, true)));
+        assertTrue(sys.equals(new SubSystem(validType, positive, negative, false)));
     }
 }
