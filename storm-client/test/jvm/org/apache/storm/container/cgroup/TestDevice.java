962b5630bba7ad38abb47075f94e863ffa52bb16
wronek97 <bianchidavide97@gmail.com>
2023-12-17 15:59:45 +0100

    Fixing TestDevice.java

--- storm-client/test/jvm/org/apache/storm/container/cgroup/TestDevice.java
+++ storm-client/test/jvm/org/apache/storm/container/cgroup/TestDevice.java
@@ -67,16 +67,11 @@ public class TestDevice {
         assertNotNull(dev);
         
         // test invalid string
-        param = "1";
-        assertThrows(ArrayIndexOutOfBoundsException.class, () -> new Device(param));
-        param = "0:";
-        assertThrows(ArrayIndexOutOfBoundsException.class, () -> new Device(param));
-        param = ":1";
-        assertThrows(NumberFormatException.class, () -> new Device(param));
-        param = "no_number";
-        assertThrows(NumberFormatException.class, () -> new Device(param));
-        param = "1:char";
-        assertThrows(NumberFormatException.class, () -> new Device(param));
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> new Device("1"));
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> new Device("0:"));
+        assertThrows(NumberFormatException.class, () -> new Device(":1"));
+        assertThrows(NumberFormatException.class, () -> new Device("no_number"));
+        assertThrows(NumberFormatException.class, () -> new Device("1:char"));
 
     }
 
